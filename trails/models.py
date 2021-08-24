2ac9a652e493c1273f1a7f569f5f4de16d2c532d
basseuph@gmail.com <basseuph@gmail.com>
2021-08-23 17:52:52 -0700

    RESTframework added, Model added and registered, urls and views added.

--- trails/models.py
+++ trails/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Hike(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    trail_name = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.trail_name[:50]
--- trails/views.py
+++ trails/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views import generic
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutUsPageView(TemplateView):
+    template_name = 'about.html'
+
+class SearchPage(TemplateView):
+    template_name = 'search.html'
